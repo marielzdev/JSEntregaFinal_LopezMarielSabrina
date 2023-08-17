52b1680879f3a2d93ab8650f8df6ced0bd777e89
Mariel Sabrina Lopez <marielslopez@gmail.com>
2023-08-17 19:35:15 -0300

    se comenta codigo JS en los diferentes archivos JS

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,36 @@
+//Array del Carrito de compras con sus funciones
+
 let carrito = [];
 
 const productoContenedor = document.getElementById("producto-contenedor");
-
+//funcion productocontenedor con lectura del evento tipo click
 productoContenedor.addEventListener("click", (e) => {
   if (e.target.classList.contains("agregar")) {
     validarProductoEnCarrito(e.target.id);
   }
 });
-
+// funcion para validacion del producto en el carrito| utlizacion de fetch | JSON |
 const validarProductoEnCarrito = (id) => {
   const estaRepetido = carrito.some((producto) => producto.id == id);
 
   if (!estaRepetido) {
-    const producto = productos.find((producto) => producto.id == id);
-    carrito.push(producto);
-    pintarProductoCarrito(producto);
-    actualizarTotalesCarrito(carrito);
+    fetch("../data/stock.json")
+      .then((response) => response.json())
+      .then((productos) => {
+        const producto = productos.find((producto) => producto.id == id);
+        carrito.push(producto);
+        pintarProductoCarrito(producto);
+        actualizarTotalesCarrito(carrito);
+      });
   } else {
-    const producto = carrito.find((producto) => producto.id == id);
-    const cantidad = document.getElementById(`cantidad${producto.id}`);
+    const producto = carrito.find((producto) => producto.id);
     producto.cantidad++;
-    cantidad.innerText = `Cantidad: ${producto.cantidad}`;
+    pintarCarrito(carrito);
     actualizarTotalesCarrito(carrito);
   }
 };
+
+//función pintar producto del carrito
 
 const pintarProductoCarrito = (producto) => {
   const contenedor = document.getElementById("carrito-contenedor");
@@ -31,13 +38,14 @@ const pintarProductoCarrito = (producto) => {
   div.classList.add("productoEnCarrito");
 
   div.innerHTML = `
-    <p>${producto.nombre}</p>
-    <p>${producto.precio}</p>
-    <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
-    <button class="btn waves-effect waves-ligth boton-eliminar" value="${producto.id}">X</button>
+  <p>${producto.nombre}</p>
+  <p>$ ${producto.precio}</p>
+  <p id=cantidad ${producto.id}>Cantidad ${producto.cantidad}</p> 
+  <button class="btn waves-effect waves-ligh boton-eliminar"value="">X</${producto.id}button>
   `;
   contenedor.appendChild(div);
 };
+//función muestra en  DOM estructura html | bucle para recorrer cada producto|
 
 const pintarCarrito = (carrito) => {
   const contenedor = document.getElementById("carrito-contenedor");
@@ -58,12 +66,16 @@ const pintarCarrito = (carrito) => {
   });
 };
 
+//funcion para eliminar productos del carrito
+
 const eliminarProductoCarrito = (id) => {
   const productoIndex = carrito.findIndex((producto) => producto.id == id);
   carrito.splice(productoIndex, 1);
   pintarCarrito(carrito);
   actualizarTotalesCarrito(carrito);
 };
+
+//función que muestra la cantidad y calcula el precio acorde a ello
 
 const actualizarTotalesCarrito = (carrito) => {
   const totalCantidad = carrito.reduce((acc, item) => acc + item.cantidad, 0);
@@ -75,23 +87,30 @@ const actualizarTotalesCarrito = (carrito) => {
   pintarTotalesCarrito(totalCantidad, totalCompra);
   guardarCarritoStorage(carrito);
 };
+//función muestra en el DOM la cantidad de productos + precio total.
 
 const pintarTotalesCarrito = (totalCantidad, totalCompra) => {
   const contadorCarrito = document.getElementById("contador-carrito");
-  const precioTotal = document.getElementById("precio-total");
+  const precioTotal = document.getElementById("precioTotal");
 
   contadorCarrito.innerText = totalCantidad;
   precioTotal.innerText = totalCompra;
 };
 
+//Esta función guarda en el localstorage del usuario lo referente a productos del carrito.
+
 const guardarCarritoStorage = (carrito) => {
   localStorage.setItem("carrito", JSON.stringify(carrito));
 };
+
+//Esta función obtiene los productos del carrito a un objeto JS
 
 const obtenerCarritoStorage = () => {
   const carritoStorage = JSON.parse(localStorage.getItem("carrito"));
   return carritoStorage;
 };
+
+//Esta función guarda todo lo anterior
 
 const cargarCarrito = () => {
   if (localStorage.getItem("carrito")) {
--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,11 @@
+// funciones referentes al modal contenedor
 const modalContenedor = document.querySelector(".modal-contenedor");
 const abrirCarrito = document.getElementById("cesta-carrito");
 const cerrarCarrito = document.getElementById("btn-cerrar-carrito");
 const modalCarrito = document.querySelector(".modal-carrito");
+const compraExitosa = document.getElementById("compraExitosa");
 
+//Estas funciones son las que hacen que nuestros botones funcionen y además se muestran en el DOM
 abrirCarrito.addEventListener("click", () => {
   modalContenedor.classList.toggle("modal-active");
 });
@@ -14,10 +17,34 @@ cerrarCarrito.addEventListener("click", () => {
 modalContenedor.addEventListener("click", () => {
   cerrarCarrito.click();
 });
-
 modalCarrito.addEventListener("click", (e) => {
   e.stopPropagation();
   if (e.target.classList.contains("boton-eliminar")) {
     eliminarProductoCarrito(e.target.value);
   }
 });
+
+//Esta función es la encargada de realizar la compra en el carrito
+compraExitosa.addEventListener("click", () => {
+  mostrarMensajeCompraExitosa()
+    .then(() => {
+      console.log("Mensaje de compra exitosa mostrado");
+    })
+    .catch((error) => {
+      console.error("Error al mostrar el mensaje:", error);
+    });
+});
+
+//Esta función asincrónica está conectada con el evento de compraExitosa porque maneja el tiempo de espera y a la vez muestra un mensaje de éxito
+const mostrarMensajeCompraExitosa = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      Swal.fire({
+        icon: "success",
+        title: "¡Bien Hecho!",
+        text: "Su compra ha sido realizada con éxito",
+      });
+      resolve(); // Resuelve la promesa después de mostrar el mensaje
+    }, 2000); // Simula un retraso de 2 segundos antes de mostrar el mensaje
+  });
+};
